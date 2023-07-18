d6c7bc7bd0cb7e67404adf64895745785660bdca
Dominik <dominik.scherm@reaction.link>
2023-07-18 16:21:56 +0200

    Update chroma vectorization.

--- src/paper_to_chroma.py
+++ src/paper_to_chroma.py
@@ -2,15 +2,17 @@ import os
 from enum import Enum
 from langchain import PromptTemplate
 from langchain.chains import RetrievalQA
+from langchain.schema import Document
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.chains.question_answering import load_qa_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 
 from constants import OPENAI_API_KEY
 from paper_loader import arxiv_id_to_latex
-from paper_to_text import extract_text_from_latex
+from latex_to_text import extract_text_from_latex
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
@@ -24,64 +26,92 @@ class EmbeddingsType(Enum):
     OPENSOURCE = "OPENSOURCE"
     OPENAI = "OPENAI"
 
+
+EMBEDDDING_FUNCTION = EmbeddingsType.OPENAI
+
+if EMBEDDDING_FUNCTION == EmbeddingsType.OPENSOURCE:
+    embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+elif EMBEDDDING_FUNCTION == EmbeddingsType.OPENAI:
+    embedding_function = OpenAIEmbeddings()
+
+DEFAULT_CONTEXT_PROMPT = """
+Given the key points and most important details section from a scientific paper, 
+generate an informative and engaging script for a video presentation. Ensure the script is concise,
+understandable for a general audience, and highlights the main takeaways:
+
+Context:
+
+{context}
+
+Key Points and Details: 
+
+{question}
+"""
+
 """
 Vectorize the paper in Chroma vector store using the given embedding function
 """
-def vectorize_paper_in_chroma(input: str, embedding_function: EmbeddingsType = EmbeddingsType.OPENSOURCE):
+def vectorize_paper_in_chroma(latex_input: str):
     # split it into chunks
     text_splitter = RecursiveCharacterTextSplitter(separators=["\section", "\subsection", "\n"], chunk_size=10000, chunk_overlap=500)
-    texts = text_splitter.split_text(input)
+    latex_chunks = text_splitter.split_text(latex_input)
 
-    # create the embedding function
-    if embedding_function == EmbeddingsType.OPENSOURCE:
-        embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-    elif embedding_function == EmbeddingsType.OPENAI:
-        embedding_function = OpenAIEmbeddings()
-    
-    # TODO: Store latex as metadata next to text
+    docs = []
+    for chunk in latex_chunks:
+        plain_text = extract_text_from_latex(chunk)
+        cleaned_text = plain_text.strip().replace('\n', '')
+
+        doc = Document(
+            page_content=cleaned_text,
+            metadata={"latex": chunk},
+        )
+
+        docs.append(doc)
     
     # load it into Chroma
-    db = Chroma.from_texts(texts, embedding_function, persist_directory="../chroma_db")
+    db = Chroma.from_documents(docs, embedding_function, persist_directory="../chroma_db")
     db.persist()
 
 
-def query_paper_by_section_name(section_name: str, embedding_function: EmbeddingsType = EmbeddingsType.OPENSOURCE):
-    # create the embedding function
-    if embedding_function == EmbeddingsType.OPENSOURCE:
-        embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-    elif embedding_function == EmbeddingsType.OPENAI:
-        embedding_function = OpenAIEmbeddings()
-
+def query_chroma(input: str, number_of_results = 5):
     # Load chroma
     db = Chroma(persist_directory="../chroma_db", embedding_function=embedding_function)
 
-    prompt_template = """
-    Given the key points and most important details of the '{context}' section from a scientific paper, 
-    generate an informative and engaging script for a video presentation. Ensure the script is concise,
-    understandable for a general audience, and highlights the main takeaways.
+    docs = db.similarity_search(input, k=number_of_results)
 
-    Key Points and Details: 
+    return docs
 
-    {question}
-    """
 
-    PROMPT = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
-    )
+def query_paper_by_prompt(question: str, context_prompt = DEFAULT_CONTEXT_PROMPT):
+    # Load chroma
+    docs = query_chroma(question, 2)
+
+    # prompt_template = context_prompt
+
+    # PROMPT = PromptTemplate(
+    #     template=prompt_template, input_variables=["context", "question"]
+    # )
 
     # Query your database here
-    chain_type_kwargs = {"prompt": PROMPT}
-    qa = RetrievalQA.from_chain_type(llm, chain_type="stuff", retriever=db.as_retriever(), chain_type_kwargs=chain_type_kwargs)
+    chain = load_qa_chain(llm, chain_type="map_reduce", verbose=True)
 
-    query = "What is in the paper about: " + section_name + "?"
-    qa.run(query)
+    return chain.run(input_documents=docs, question=question)
 
 
 if __name__ == "__main__":
-    # load the document and split it into chunks
-    paper = arxiv_id_to_latex("1706.03762")
+    # load the document and split it into chunks 
+    # paper = arxiv_id_to_latex("1706.03762")
 
-    text = extract_text_from_latex(paper)
+    # vectorize_paper_in_chroma(paper)
 
-    vectorize_paper_in_chroma(text)
+    # Query chroma
+    # docs = query_chroma("position-wise feed-forward networks", 1)
+    # print(len(docs))
+
+    # Query paper by prompt
+    answer = query_paper_by_prompt("What is this paper about?")
+    print(answer)
+
+
+
 
