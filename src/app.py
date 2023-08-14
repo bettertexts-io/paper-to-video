05baf38ce3ce636c304e66d66b7ee92718f2fce9
Dominik <dominik.scherm@reaction.link>
2023-08-14 23:58:07 +0200

    Refactor main pipeline.

--- src/app.py
+++ src/app.py
@@ -1,161 +1,106 @@
-import json
 import os
-import sys
-
+import logging
 from dotenv import load_dotenv
-
+from paper_loader import paper_id_to_latex
 from chroma import vectorize_latex_in_chroma
-from constants import MOCK_SUMMARY
 from langchain_summarize import (
     COMBINE_SUMMARY_PROMPT,
-    CRAFT_SCRIPT_PROMPT,
     MAP_SUMMARY_PROMPT,
     summarize_by_map_reduce,
-    summarize_by_refine,
 )
-from paper_loader import paper_id_to_latex
-from script_refinement import generate_script, generate_script_scenes
-from stock_footage import generate_stock_footage
 from summary_to_script import generate_barebone_script
-from text_alignments_to_captions import  generate_video_captions
-from text_to_voice import generate_script_audio_pieces, text_to_voice
-from tmp import tmp_loader, tmp_path, tmp_saver
+from script_refinement import generate_script_scenes
+from text_to_voice import generate_script_audio_pieces
+from stock_footage import generate_stock_footage
+from text_alignments_to_captions import generate_video_captions
 from vid_render import render_vid
+from tmp import tmp_loader, tmp_path, tmp_saver
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 load_dotenv()
-
 
 SKIP_VECTORIZATION = True
 
+def fetch_paper(paper_id):
+    logging.info(f"Fetching paper with id {paper_id}")
+    return paper_id_to_latex(paper_id)
 
-def paper_2_video(paper_id, MOCK_SUMMARY=None):
+def vectorize_paper(latex):
+    if not SKIP_VECTORIZATION:
+        logging.info(f"Vectorizing paper")
+        vectorize_latex_in_chroma(latex)
+
+def get_summary(paper_id, latex):
+    summary = tmp_loader(paper_id=paper_id, kind="summary", save_type="str")
+    if summary is None:
+        logging.info(f"Generating summary")
+        summary = summarize_by_map_reduce(latex, MAP_SUMMARY_PROMPT, COMBINE_SUMMARY_PROMPT)
+        tmp_saver(paper_id=paper_id, kind="summary", data=summary, save_type="str")
+    else:
+        logging.info("Using cached summary")
+    return summary
+
+def generate_script_from_summary(paper_id, summary):
+    barebone_script_json = tmp_loader(paper_id=paper_id, kind="barebone_script", save_type="json")
+    if barebone_script_json is None:
+        logging.info(f"Generating barebone video script")
+        barebone_script_json = generate_barebone_script(summary)
+        tmp_saver(paper_id=paper_id, kind="barebone_script", data=barebone_script_json, save_type="json")
+    else:
+        logging.info("Using cached barebone script")
+    return barebone_script_json
+
+def enrich_script(paper_id, barebone_script_json):
+    script_with_scenes = tmp_loader(paper_id=paper_id, kind="script_with_scenes", save_type="json")
+    if script_with_scenes is None:
+        logging.info(f"Generating detailed context for each section")
+        script_with_scenes = {"sections": []}
+        for section in barebone_script_json["sections"]:
+            scenes = generate_script_scenes(section)
+            script_with_scenes["sections"].append(section)
+            script_with_scenes["sections"][-1]["scenes"] = scenes
+        tmp_saver(paper_id=paper_id, kind="script_with_scenes", data=script_with_scenes, save_type="json")
+    else:
+        logging.info("Using cached enriched script")
+    return script_with_scenes
+
+def generate_audio(paper_id, script_with_scenes):
+    logging.info(f"Generating script audio pieces")
+    return generate_script_audio_pieces(paper_id=paper_id, script=script_with_scenes)
+
+def get_stock_footage(paper_id, script_with_scenes):
+    logging.info(f"Generating stock footage")
+    return generate_stock_footage(paper_id=paper_id, script=script_with_scenes)
+
+def generate_captions(paper_id, script_with_scenes):
+    logging.info(f"Generating video captions")
+    return generate_video_captions(paper_id=paper_id, script=script_with_scenes)
+
+def render_video(stock_footage_paths, audio_paths, video_caption_filenames, paper_id):
+    logging.info(f"Generating video")
+    render_vid(
+        animation_filenames=stock_footage_paths,
+        voice_filenames=audio_paths,
+        video_caption_filenames=video_caption_filenames,
+        output_filename=tmp_path(paper_id=paper_id, kind="output"),
+    )
+
+def paper_2_video(paper_id):
     try:
-        print(f"Fetching paper with id {paper_id}")
-        latex = paper_id_to_latex(paper_id)
+        latex = fetch_paper(paper_id)
+        vectorize_paper(latex)
+        summary = get_summary(paper_id, latex)
+        barebone_script = generate_script_from_summary(paper_id, summary)
+        enriched_script = enrich_script(paper_id, barebone_script)
+        audio_pieces = generate_audio(paper_id, enriched_script)
+        stock_footage_paths = get_stock_footage(paper_id, enriched_script)
+        captions = generate_captions(paper_id, enriched_script)
+        render_video(stock_footage_paths, audio_pieces, captions, paper_id)
     except Exception as e:
-        print(f"Failed to fetch the paper with id {paper_id}: {e}")
-        return
-
-    try:
-        # Check if paper is already vectorized otherwise store vectors in chroma db
-        print(f"Vectorizing paper")
-        if not SKIP_VECTORIZATION:
-            vectorize_latex_in_chroma(latex)
-    except Exception as e:
-        print(f"Failed to vectorize paper")
-        return
-
-    try:
-        # Summarize the paper using langchain map_reduce summarization
-        # The summary includes a list of the sections of the paper at the end
-        summary = tmp_loader(paper_id=paper_id, kind="summary", save_type="str")
-
-        if summary is None:
-            print(f"Generating summary")
-            summary = summarize_by_map_reduce(latex, MAP_SUMMARY_PROMPT, COMBINE_SUMMARY_PROMPT)
-            tmp_saver(paper_id=paper_id, kind="summary", data=summary, save_type="str")
-        else:
-            print("Using cached summary")
-
-    except Exception as e:
-        print(f"Failed to summarize the paper: {e}")
-        return
-
-    try:
-        # Check the path for the barebone script
-
-        barebone_script_json = tmp_loader(
-            paper_id=paper_id, kind="barebone_script", save_type="json"
-        )
-
-        if barebone_script_json is None:
-            print(f"Generating barebone video script")
-            # Turn the summary into a barebone video script structure with approximated lengths of the sections
-            barebone_script_json = generate_barebone_script(summary)
-            tmp_saver(
-                paper_id=paper_id,
-                kind="barebone_script",
-                data=barebone_script_json,
-                save_type="json",
-            )
-        else:
-            print("Using cached barebone script")
-
-    except Exception as e:
-        print(f"Failed to create video script: {e}")
-
-    try:
-        # Define the path for the enriched script
-        script_with_scenes = tmp_loader(
-            paper_id=paper_id, kind="script_with_scenes", save_type="json"
-        )
-
-        # Otherwise, generate the enriched script and store it
-        if script_with_scenes is None:
-            print(f"Generating detailed context for each section")
-            # Generate a detailed summary for each section of the generated script structure
-            # Feed in the generated barebone script
-            # enriched_script_json = generate_script(barebone_script_json)
-            script_with_scenes = {"sections": []}
-            for section in barebone_script_json["sections"]:
-                # TODO: Pass last two sections to the script refinement 
-                print(section)
-                scenes = generate_script_scenes(section)
-                script_with_scenes["sections"].append(section)
-                script_with_scenes["sections"][-1]["scenes"] = scenes
-
-            print(script_with_scenes)
-            tmp_saver(
-                paper_id=paper_id,
-                kind="script_with_scenes",
-                data=script_with_scenes,
-                save_type="json",
-            )
-        else:
-            print("Using cached enriched script")
-
-    except Exception as e:
-        print(f"Failed to generate detailed summary: {e}")
-        return
-
-    try:
-        # Convert script into audio pieces
-        print(f"Generating script audio pieces")
-        audio_paths = generate_script_audio_pieces(
-            paper_id=paper_id, script=script_with_scenes
-        )
-
-    except Exception as e:
-        print(f"Failed to generate audio pieces: {e}")
-        return
-    
-    try: 
-        stock_footage_paths = generate_stock_footage(paper_id=paper_id, script=script_with_scenes)
-    except Exception as e:
-        print(f"Failed to generate stock footage: {e}")
-        return
-    
-    try: 
-        print(f"Generating video captions")
-        video_caption_filenames = generate_video_captions(paper_id=paper_id, script=script_with_scenes)
-    except Exception as e:
-        print(f"Failed to generate video captions: {e}")
-        return
-
-    try:
-        # Convert audio pieces into video
-        print(f"Generating video")
-        render_vid(
-            animation_filenames=stock_footage_paths,
-            voice_filenames=audio_paths,
-            video_caption_filenames=video_caption_filenames,
-            output_filename=tmp_path(paper_id=paper_id, kind="output"),
-        )
-    except Exception as e:
-        print(f"Failed to create video: {e}")
-        return
+        logging.error(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    print("Starting app")
-    paper_2_video("1706.03762", MOCK_SUMMARY=MOCK_SUMMARY)
+    logging.info("Starting pipeline...")
+    paper_2_video("1706.03762")
